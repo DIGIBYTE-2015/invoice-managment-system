774234ee9a820f91e0ea75da4f322e4f2d984257
Davide Zuppiroli <zuppirolidavide618@gmail.com>
2020-07-01 15:08:40 +0200

    first commit

---
+++ public/pdf.php
@@ -0,0 +1,12 @@
+<?php
+//pdf.php;
+require_once '../vendor/autoload.php';
+use Dompdf\Dompdf;
+
+class Pdf extends Dompdf{
+ public function __construct() {
+        parent::__construct();
+    }
+}
+
+?>
